8e11c15e13e352386b47e1c5607a22d0cb14e728
Neto test <test@neto.com>
2023-01-30 18:43:43 +0300

    New menager

--- src/main/java/InformationPoster.java
+++ src/main/java/InformationPoster.java
@@ -9,13 +9,13 @@ public class InformationPoster {
 
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
 
 
 
